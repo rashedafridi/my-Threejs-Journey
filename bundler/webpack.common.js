a0aae3d617e9b7d87503767c25873ec99532bed3
Rashed Afridi <rashedafridi@gmail.com>
2023-07-13 04:01:45 +0600

    load shaders file

--- bundler/webpack.common.js
+++ bundler/webpack.common.js
@@ -76,7 +76,29 @@ module.exports = {
                 {
                     filename: 'assets/fonts/[hash][ext]'
                 }
-            }
+            },
+
+            // Shaders  
+            {
+                test: /\.(glsl|vs|fs|vert|frag)$/,
+                type: 'asset/source',
+                generator:
+                {
+                    filename: 'assets/images/[hash][ext]'
+                }
+            },
+            // {
+            //     test: /\.(glsl|vs|fs|vert|frag)$/,
+            //     exclude: /node_modules/,
+            //     type: 'asset/source',
+            //     use:[
+            //         'row-loader'
+            //     ]
+            //     // generator:
+            //     // {
+            //     //     filename: 'assets/images/[hash][ext]'
+            //     // }
+            // }
         ]
     }
 }
